8bd60b387d096ab3c28a9b85c51341dfc02680e0
Exulansis <rusu.eugeniu@gmx.de>
2020-05-14 18:56:31 +0300

    Updated to use the latest SDK version

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,55 @@
-import * as hapi from "hapi";
-import { sdkPlugin, SDKOptions } from "hapi-jolocom-plugin";
-import config from "../config.json";
+import * as hapi from 'hapi'
+import { sdkPlugin } from "hapi-jolocom-plugin"
+import { FilePasswordStore } from "@jolocom/sdk"
+import { JolocomTypeormStorage } from "@jolocom/sdk-storage-typeorm"
+const typeorm = require("typeorm")
+
+const typeormConfig = {
+  type: 'sqlite',
+  database: __dirname + '/db.sqlite3',
+  logging: ['error', 'warn', 'schema'],
+  entities: [ ...require('@jolocom/sdk-storage-typeorm').entityList ],
+  migrations: [__dirname + '/migrations/*.ts'],
+  migrationsRun: true,
+  synchronize: true,
+  cli: {
+    migrationsDir: __dirname + '/migrations',
+  }
+}
 
 export const init = async () => {
+  const passwordStore = new FilePasswordStore(__dirname+'/password.txt')
+  const connection = await typeorm.createConnection(typeormConfig)
+  const storage = new JolocomTypeormStorage(connection)
+
   const server = new hapi.Server({
     host: "localhost",
     port: process.env.PUBLIC_PORT || 9000,
+    debug: {
+      request: "*"
+    }
   });
 
   await server.register({
     plugin: sdkPlugin,
-    options: (config as unknown) as SDKOptions,
+    options: { identityData: {
+      storage, passwordStore
+    },
+    verifierOptions: [{
+      name: "TEST",
+      requirements: [
+        {
+          type: ["email"],
+          constraints: []
+        }
+      ],
+    }
+  ],
+    }
   });
 
   await server.start();
+  console.log("running")
 };
 
 init();
